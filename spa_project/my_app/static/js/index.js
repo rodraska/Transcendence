77528cf4c240e3aaba3e786ae262ff3f1f7ced11
Sofia Leong <sofia.leong@tui.com>
2025-02-01 15:38:36 +0000

    feature: basic login buttons and forms

--- spa_project/my_app/static/js/index.js
+++ spa_project/my_app/static/js/index.js
@@ -4,14 +4,20 @@ import HomePage from './home/home_page.js'
 import HeaderBar from "./header/header.js";
 import PongPage from "./pong/pong.js";
 import CurvePage from "./curve/curve.js"
+import LoginButtons from "./login/login.js";
+import LoginForm from "./login/login_form.js";
+import RegistrationForm from "./login/registration_form.js";
 
 Route.subscribe('/', HomePage);
 Route.subscribe('/user', UserProfile);
 Route.subscribe('/header', HeaderBar);
 Route.subscribe('/pong', PongPage);
 Route.subscribe('/curve', CurvePage);
-Route.go('/header');
-Route.go('/user');
+Route.subscribe('/login', LoginButtons);
+Route.subscribe('/login_form', LoginForm);
+Route.subscribe('/registration_form', RegistrationForm);
+// Route.go('/login_form')
+Route.go('/login')
 
 
 //este file precisa de ser revisto,nao podemos ter as routes assim, para a Carla quando estiveres a testar tens de comentar o Route.go('/user')
