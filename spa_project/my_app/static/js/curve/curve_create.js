e290d2144eb6ef209aa8c707cc1ef89fdc5c622c
Rodrigo Carvalho <rreiscarvalho1999@gmail.com>
2025-02-02 15:37:27 +0000

    Create more pages

---
+++ spa_project/my_app/static/js/curve/curve_create.js
@@ -0,0 +1,14 @@
+import Component from "../spa/component.js"
+
+export default class CurveCreate extends Component
+{
+    constructor()
+    {
+        super('static/html/curve_create.html')
+    }
+
+    onInit()
+    {
+        
+    }
+}
---
+++ spa_project/my_app/static/js/pong/pong_main.js
@@ -0,0 +1,14 @@
+import Component from "../spa/component.js"
+
+export default class PongMain extends Component
+{
+    constructor()
+    {
+        super('static/html/pong_main.html')
+    }
+
+    onInit()
+    {
+
+    }
+}
