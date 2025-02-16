fa15d44be3acb930e4802e00891b5c3628659ca9
Rodrigo Carvalho <rreiscarvalho1999@gmail.com>
2025-02-16 15:53:14 +0000

    Integrate my code in docker

---
+++ docker/srcs/django/my_app/static/js/pong/pong_join.js
@@ -0,0 +1,14 @@
+import Component from "../spa/component.js"
+
+export default class PongJoin extends Component
+{
+    constructor()
+    {
+        super('static/html/pong_join.html')
+    }
+
+    onInit()
+    {
+
+    }
+}
