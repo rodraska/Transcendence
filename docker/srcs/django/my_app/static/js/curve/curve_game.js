079a6b1edd5d7deab9f7194d831862a269066e6d
rodraska <rreiscarvalho1999@gmail.com>
2025-04-09 22:47:56 +0100

    Add new pong and curve file to index

--- docker/srcs/django/my_app/static/js/curve/curve_game.js
+++ docker/srcs/django/my_app/static/js/curve/curve_game.js
@@ -1,6 +1,6 @@
 import Component from "../spa/component.js"
-import { initializeCurveGameProperties } from "./curve_game_properties.js"
-import "./curve_events.js"
+//import { initializeCurveGameProperties } from "./curve_game_properties.js"
+//import "./curve_events.js"
 
 class CurveGame extends Component
 {
@@ -8,17 +8,17 @@ class CurveGame extends Component
     {
         super('static/html/curve_game.html');
 
-        this.matchData = window.currentMatchData;
+        /*this.matchData = window.currentMatchData;
 
-        Object.assign(this, initializeCurveGameProperties());
+        Object.assign(this, initializeCurveGameProperties());*/
     }
 
     onInit() {
-        window.curve_game = this;
+        /*window.curve_game = this;
         this.getCurveHtmlElements(0);
         this.setupCurveSocket();
         this.sendMatchData(0);
-        this.curveGameControlEvents();
+        this.curveGameControlEvents();*/
     }
 }
 
