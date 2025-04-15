60a63c1da3a6d3b01038dc9d80b989e549aac29a
Sofia Leong <sofia.leong@tui.com>
2025-04-15 22:24:37 +0100

    feat: add user validation in registration form

--- docker/srcs/django/my_app/static/js/login/registration_form.js
+++ docker/srcs/django/my_app/static/js/login/registration_form.js
@@ -2,6 +2,7 @@ import Component from "../spa/component.js";
 import { showToast } from "../utils/toast.js";
 import Route from "../spa/route.js";
 
+const isAlpha = str => /^[a-zA-Z]*$/.test(str);
 class RegistrationForm extends Component {
   constructor() {
     super("static/html/registration_form.html");
@@ -9,9 +10,41 @@ class RegistrationForm extends Component {
 
   onInit() {
     const form = this.querySelector("form");
+    const usernameInput = form.querySelector("#username");
+    const usernameError = form.querySelector("#username_error");
+    const submitButton = form.querySelector("button[type='submit']");
+
+    submitButton.disabled = true;
+
+    const validateUsername = () => {
+      const value = usernameInput.value.trim();
+      let errorMsg = "";
+
+      if (value.length > 0 && value.length > 8) {
+        errorMsg = "Username is too long.";
+      } else if (value.length < 3) {
+        errorMsg = "Username is too short.";
+      } else if (!isAlpha(value)) {
+        errorMsg = "Username can only contain letters.";
+      }
+
+      if (errorMsg) {
+        usernameError.textContent = errorMsg;
+        usernameError.classList.remove("d-none");
+        submitButton.disabled = true;
+      } else {
+        usernameError.textContent = "";
+        usernameError.classList.add("d-none");
+        submitButton.disabled = false;
+      }
+    };
+
+    usernameInput.addEventListener("blur", validateUsername);
+
     form.addEventListener("submit", (e) => {
       e.preventDefault();
-      const username = form.querySelector("#username").value;
+
+      const username = usernameInput.value.trim();
       const email = form.querySelector("#email").value;
       const password = form.querySelector("#password").value;
       const confirm_password = form.querySelector("#confirm-password").value;
